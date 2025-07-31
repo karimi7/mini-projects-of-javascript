a6bbedc099900f396240ae5cf09aed1532a6dc72
Mohaddese Karimi <mhaddese@gmail.com>
2025-08-01 00:00:57 +0330

    18 - Image Zoom

---
+++ js projects/18 - Image Zoom/app.js
@@ -0,0 +1,18 @@
+const imgContainer = document.querySelector('.container');
+const img = document.querySelector('img');
+
+imgContainer.addEventListener('mousemove', (event) => {
+    console.log(event.clientX, event.clientY, event.target.offsetLeft, event.target.offsetTop
+    );
+
+    const xPos = event.clientX - event.target.offsetLeft;
+    const yPos = event.clientY - event.target.offsetTop;
+
+    img.style.transformOrigin = `${xPos}px ${yPos}px`;
+    img.style.transform = 'scale(2)';
+});
+
+imgContainer.addEventListener('mouseleave', () => {
+    img.style.transformOrigin = `center`;
+    img.style.transform = 'scale(1)';
+});
