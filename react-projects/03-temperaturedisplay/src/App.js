bd5c86dc651498712aadae7dfb069920a2b21d96
Mohaddese Karimi <mhaddese@gmail.com>
2025-10-23 23:30:00 +0330

    03 - Temperature Display

---
+++ react-projects/03-temperaturedisplay/src/App.js
@@ -0,0 +1,12 @@
+import Temp from './components/Temp'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-projects/03-temperaturedisplay/src/components/Temp.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './Temp.css';
+
+class Temp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            temp: 10,
+            tempClass: 'cold',
+        };
+
+        this.addToTemp = this.addToTemp.bind(this);
+        this.minusFromTemp = this.minusFromTemp.bind(this);
+    }
+
+    addToTemp() {
+        if (this.state.temp >= 30) {
+            return false;
+        }
+
+        this.setState((prevState) => {
+            return { temp: prevState.temp + 1 };
+        });
+
+        if (this.state.temp >= 15) {
+            this.setState({ tempClass: 'hot' });
+        }
+    }
+
+    minusFromTemp() {
+        if (this.state.temp <= 0) {
+            return false;
+        }
+
+        this.setState((prevState) => {
+            return { temp: prevState.temp - 1 };
+        });
+
+        if (this.state.temp <= 15) {
+            this.setState({ tempClass: 'cold' });
+        }
+    }
+
+    render() {
+        return (
+            <div className="app-container">
+                <div className="temperature-display-container">
+                    <div
+                        className={`temperature-display ${this.state.tempClass}`}
+                    >
+                        {this.state.temp}°C
+                    </div>
+                </div>
+                <div className="button-container">
+                    <button onClick={this.addToTemp}>+</button>
+                    <button onClick={this.minusFromTemp}>-</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Temp;
