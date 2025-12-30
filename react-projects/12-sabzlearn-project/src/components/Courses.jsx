7566ea7cffe49b526e644add9cf726f6594c78f0
Mohaddese Karimi <mhaddese@gmail.com>
2025-12-30 21:14:49 +0330

    12 - Sabzlearn Project: update

--- react-projects/12-sabzlearn-project/src/components/Courses.jsx
+++ react-projects/12-sabzlearn-project/src/components/Courses.jsx
@@ -3,9 +3,14 @@ import coursesData from '../CoursesData';
 import Course from './Course';
 import { Container } from 'react-bootstrap';
 import './Responsive.css';
+import { useLocation } from 'react-router';
 
 export default function Courses() {
     const [courses] = useState(coursesData);
+
+    let location = useLocation();
+    console.log(location);
+
     return (
         <>
             <Container>
